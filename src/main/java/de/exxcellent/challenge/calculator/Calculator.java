0596546c37f0991a0b2f608e1234bd6dc77ee447
Vincenzo Brancaccio <vincenzo.brancaccio@alumni.uni-ulm.de>
2023-01-19 16:53:51 +0100

    Calculator: find smallest diff betw. 2 values in a list of HashMaps

--- src/main/java/de/exxcellent/challenge/calculator/Calculator.java
+++ src/main/java/de/exxcellent/challenge/calculator/Calculator.java
@@ -1,5 +1,8 @@
 package de.exxcellent.challenge.calculator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * 
  * Class Calculator provides basic mathematical operations
@@ -23,6 +26,42 @@ public class Calculator {
 	public int calculateDifference(int x, int y) {
 		int diff = x - y;
 		return diff;
+	}
+	
+	/**
+	 * 
+	 * Finds the smallest difference between two integers
+	 * from a list of HashMaps (key: String, value: Int)
+	 * when two keys are given, and returns it.
+	 * Method is not finished yet.
+	 * 
+	 * @param listOfHashMaps (list of HashMaps (String, Int)
+	 * @param key1 (String, name of first key and operand)
+	 * @param key2 (String, name of second key and operand)
+	 * @return
+	 */
+	public int findSmallestDifference(ArrayList<HashMap<String, Integer>> listOfHashMaps, String key1, String key2) {
+		int smallestDiff = Integer.MAX_VALUE;
+		
+		if (!listOfHashMaps.isEmpty()) {
+			for (HashMap<String, Integer> hashMap : listOfHashMaps) {
+				int x = 0;
+				int y = 0;
+				if (hashMap.containsKey(key1)) {
+					x = hashMap.get(key1);
+				}
+				if (hashMap.containsKey(key2)) {
+					y = hashMap.get(key2);
+				}
+				int diff = calculateDifference(x, y);
+
+				if (diff < smallestDiff) {
+					smallestDiff = diff;
+				}
+			}
+		}
+		
+		return smallestDiff;
 	}
 
 }
--- src/test/java/de/exxcellent/challenge/calculator/CalculatorTest.java
+++ src/test/java/de/exxcellent/challenge/calculator/CalculatorTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CalculatorTest {
 	
 	Calculator calc;
@@ -19,6 +22,34 @@ public class CalculatorTest {
 	void testDifference() {
 		assertEquals(5, calc.calculateDifference(10, 5));
 		assertNotEquals(5, calc.calculateDifference(11, 5));
+	}
+	
+	@Test
+	void testSmallestDifference() {
+		ArrayList<HashMap<String, Integer>> listOfTestMaps = new ArrayList<HashMap<String, Integer>>();
+		HashMap<String, Integer> testMap1 = new HashMap<>();
+		
+		testMap1.put("key1", 1);
+		testMap1.put("key2", 2);
+		testMap1.put("key3", 3);
+		
+		HashMap<String, Integer> testMap2 = new HashMap<>();
+		
+		testMap2.put("key1", 5);
+		testMap2.put("key2", 10);
+		testMap2.put("key3", 20);
+		
+		HashMap<String, Integer> testMap3 = new HashMap<>();
+		
+		testMap3.put("key1", 10);
+		testMap3.put("key2", 20);
+		testMap3.put("key3", 30);
+		
+		listOfTestMaps.add(testMap1);
+		listOfTestMaps.add(testMap2);
+		listOfTestMaps.add(testMap3);
+		
+		assertEquals(1, calc.findSmallestDifference(listOfTestMaps, "key2", "key1"));
 	}
 
 }
